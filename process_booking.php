b415a4367e659260f6ec2cf096d00db19faa32fa
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-02 23:00:10 +0300

    Register kai alla

--- process_booking.php
+++ process_booking.php
@@ -1,10 +1,35 @@
 <?php
-// Εδώ θα μπορούσατε να επεξεργαστείτε τα δεδομένα που λαμβάνετε από τη φόρμα κράτησης.
-// Συμπεριλάβετε κάθε λειτουργία που απαιτείται για την αναζήτηση πτήσεων.
 
-// Επιλογή πτήσεων από τη βάση δεδομένων ή άλλη πηγή.
+// Σύνδεση στη βάση δεδομένων
+$servername = "localhost";
+$username = "root"; // Όνομα χρήστη root
+$password = ""; // Κωδικός πρόσβασης (αφήστε κενό αν δεν έχετε ορίσει κωδικό)
+$dbname = "swiftskies"; // Όνομα της βάσης δεδομένων
 
-// Στη συνέχεια, προωθήστε το χρήστη στη σελίδα αποτελεσμάτων πτήσεων.
-header("Location: flight_search_results.html");
-exit;
+// Παίρνουμε τα δεδομένα από την υποβολή της φόρμας
+$surname = $_POST['surname']; // Προσαρμόστε το 'surname' ανάλογα με το όνομα του πεδίου στη φόρμα
+$name = $_POST['name'];
+$address = $_POST['address'];
+$phone = $_POST['phone'];
+
+// Δημιουργία σύνδεσης
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Έλεγχος σύνδεσης
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Εισαγωγή δεδομένων στη βάση δεδομένων
+$sql = "INSERT INTO Passenger (SURNAME, NAME, ADDRESS, PHONE) VALUES ('$surname', '$name', '$address', '$phone')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Κλείσιμο σύνδεσης
+$conn->close();
+
 ?>
---
+++ registerform.php
@@ -0,0 +1,24 @@
+<?php
+    $fullName = $_POST['fullName'];
+    $email = $_POST['email'];
+    $gender = $_POST['gender'];
+    $phone = $_POST['phone'];
+
+    $conn = new mysqli('localhost', 'root', '', 'swiftskies');
+
+    if($conn->connect_error){
+        die('Connection Failed: '.$conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO registration (fullName, email, phone, gender) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $fullName, $email, $phone, $gender);
+        $stmt->execute();
+        echo "Registration Successfully...";
+        $stmt->close();
+        $conn->close();
+    }
+?>
+
+<script>
+    // Ανακατεύθυνση στην αρχική σελίδα μετά την επιτυχή εγγραφή
+    window.location.href = "index.html";
+</script>
