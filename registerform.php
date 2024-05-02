d6d7a00c6d3397066beea1d87333514114cf267e
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-03 00:29:11 +0300

    okoko

--- registerform.php
+++ registerform.php
@@ -24,6 +24,10 @@ if(!empty($_POST['fullName']) && !empty($_POST['email']) && !empty($_POST['role'
             $stmt->close();
         }
         $conn->close();
+
+        // Ανακατεύθυνση στην αρχική σελίδα μετά την επιτυχή εγγραφή
+        header("Location: index.html");
+        exit();
     }
 } else {
     echo "Please fill out all the fields in the form.";
