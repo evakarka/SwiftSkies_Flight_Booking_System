8a9e8d45fd76c6271dc8467149aee0dc5522d745
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-09 18:46:45 +0300

    back

--- registerform.php
+++ registerform.php
@@ -1,39 +1,24 @@
 <?php
-if (!empty($_POST['fullName']) && !empty($_POST['email']) && !empty($_POST['role']) && !empty($_POST['phone']) && !empty($_POST['password'])) {
     $fullName = $_POST['fullName'];
     $email = $_POST['email'];
-    $role = $_POST['role'];
+    $gender = $_POST['gender'];
     $phone = $_POST['phone'];
-    $password = $_POST['password'];
 
     $conn = new mysqli('localhost', 'root', '', 'swiftskies');
 
-    if ($conn->connect_error) {
-        die('Connection Failed: ' . $conn->connect_error);
+    if($conn->connect_error){
+        die('Connection Failed: '.$conn->connect_error);
     } else {
-        // Κρυπτογράφηση του κωδικού πρόσβασης
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-        if ($role === 'staff' || $role === 'pilot' || $role === 'purser') {
-            $stmt = $conn->prepare("INSERT INTO admin_approval (fullName, email, phone, role, password) VALUES (?, ?, ?, ?, ?)");
-            $stmt->bind_param("sssss", $fullName, $email, $phone, $role, $hashedPassword);
-            $stmt->execute();
-            echo "Your registration requires admin approval. Please wait for confirmation.";
-            $stmt->close();
-        } else {
-            $stmt = $conn->prepare("INSERT INTO registration (fullName, email, phone, role, password) VALUES (?, ?, ?, ?, ?)");
-            $stmt->bind_param("sssss", $fullName, $email, $phone, $role, $hashedPassword);
-            $stmt->execute();
-            echo "Registration Successfully...";
-            $stmt->close();
-        }
+        $stmt = $conn->prepare("INSERT INTO registration (fullName, email, phone, gender) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $fullName, $email, $phone, $gender);
+        $stmt->execute();
+        echo "Registration Successfully...";
+        $stmt->close();
         $conn->close();
-
-        // Ανακατεύθυνση στην αρχική σελίδα μετά την επιτυχή εγγραφή
-        header("Location: index.html");
-        exit();
     }
-} else {
-    echo "Please fill out all the fields in the form.";
-}
 ?>
+
+<script>
+    // Ανακατεύθυνση στην αρχική σελίδα μετά την επιτυχή εγγραφή
+    window.location.href = "index.html";
+</script>
