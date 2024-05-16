47a49761f20e2c09923ae00324f49d0c336ae1c6
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-16 18:22:33 +0300

    Admin Panel_

---
+++ adminpanel/dashboard/flights.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Airline Company - Passengers</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+
+    <style>
+        
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        
+        .table th {
+            border-bottom: solid 2px black;
+            padding: 3px;
+        }
+        
+        .table td {
+            padding: 3px;
+            border: solid 1px rgb(255, 255, 255);
+            transition: background-color 0.3s ease; 
+        }
+        
+        .table tbody tr:hover {
+            background-color: antiquewhite;
+        }
+        
+        .table tbody tr:hover td {
+            background-color: #9dd1f1; 
+        }
+        
+
+    </style>
+
+</head>
+
+<body>
+    <!-- =============== Navigation ================ -->
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="/index.html">
+                        <span class="icon">
+                            <img src="assets/imgs/logo.png" alt="logo" style="height: 30px;">
+                        </span>
+                        <span class="title">SwiftSkies</span>
+                        
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.html">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="passengers.php">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">Passengers</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="flights.php">
+                        <span class="icon">
+                            <ion-icon name="airplane-outline"></ion-icon>
+                        </span>
+                        <span class="title">Flights</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="airplanes.php">
+                        <span class="icon">
+                            <ion-icon name="airplane-outline" style="transform: rotate(-45deg);"></ion-icon>
+                        </span>
+                        <span class="title">Airplanes</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="staff.php">
+                        <span class="icon">
+                            <ion-icon name="person-outline">></ion-icon>
+                        </span>
+                        <span class="title">Staff</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="city.php">
+                        <span class="icon">
+                            <ion-icon name="globe-outline">></ion-icon>
+                        </span>
+                        <span class="title">City</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="adminprofile.php">
+                        <span class="icon">
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                        </span>
+                        <span class="title">Admin Profile</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="help.php">
+                        <span class="icon">
+                            <ion-icon name="help-outline"></ion-icon>
+                        </span>
+                        <span class="title">Help</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="setting.php">
+                        <span class="icon">
+                            <ion-icon name="settings-outline"></ion-icon>
+                        </span>
+                        <span class="title">Settings</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="password.php">
+                        <span class="icon">
+                            <ion-icon name="lock-closed-outline"></ion-icon>
+                        </span>
+                        <span class="title">Password</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="signout.php">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="main">
+        <div class="container">
+    <h2>Flights Information</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>NUM</th>
+          <th>FLIGHTNUM</th>
+          <th>ORIGIN</th>
+          <th>DEST</th>
+          <th>DATE</th>
+          <th>ARR-TIME</th>
+          <th>DEP-TIME</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Εδώ θα προστεθούν δυναμικά οι πληροφορίες πτήσεων -->
+      </tbody>
+    </table>
+  </div>
+    </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+  </script>
+
+<script src="assets/js/main.js"></script>
+
+<!-- ====== ionicons ======= -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
---
+++ adminpanel/dashboard/passengers.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Airline Company - Passengers</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+
+    <style>
+        
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        
+        .table th {
+            border-bottom: solid 2px black;
+            padding: 3px;
+        }
+        
+        .table td {
+            padding: 3px;
+            border: solid 1px rgb(255, 255, 255);
+            transition: background-color 0.3s ease; 
+        }
+        
+        .table tbody tr:hover {
+            background-color: antiquewhite;
+        }
+        
+        .table tbody tr:hover td {
+            background-color: #9dd1f1; 
+        }
+        
+
+    </style>
+
+</head>
+
+<body>
+    <!-- =============== Navigation ================ -->
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="/index.html">
+                        <span class="icon">
+                            <img src="assets/imgs/logo.png" alt="logo" style="height: 30px;">
+                        </span>
+                        <span class="title">SwiftSkies</span>
+                        
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.html">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="passengers.php">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">Passengers</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="flights.php">
+                        <span class="icon">
+                            <ion-icon name="airplane-outline"></ion-icon>
+                        </span>
+                        <span class="title">Flights</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="airplanes.php">
+                        <span class="icon">
+                            <ion-icon name="airplane-outline" style="transform: rotate(-45deg);"></ion-icon>
+                        </span>
+                        <span class="title">Airplanes</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="staff.php">
+                        <span class="icon">
+                            <ion-icon name="person-outline">></ion-icon>
+                        </span>
+                        <span class="title">Staff</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="city.php">
+                        <span class="icon">
+                            <ion-icon name="globe-outline">></ion-icon>
+                        </span>
+                        <span class="title">City</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="adminprofile.php">
+                        <span class="icon">
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                        </span>
+                        <span class="title">Admin Profile</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="help.php">
+                        <span class="icon">
+                            <ion-icon name="help-outline"></ion-icon>
+                        </span>
+                        <span class="title">Help</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="setting.php">
+                        <span class="icon">
+                            <ion-icon name="settings-outline"></ion-icon>
+                        </span>
+                        <span class="title">Settings</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="password.php">
+                        <span class="icon">
+                            <ion-icon name="lock-closed-outline"></ion-icon>
+                        </span>
+                        <span class="title">Password</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="signout.php">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="main">
+            <div class="container">
+                <h2>Passengers Information</h2>
+                <table class="table">
+                <thead>
+                    <tr>
+                    <th>ERNAME</th>
+                    <th>SURNAME</th>
+                    <th>NAME</th>
+                    <th>ADDRESS</th>
+                    <th>PHONE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    <tr>
+                    <td>1</td>
+                    <td>Karka</td>
+                    <td>Evangelia</td>
+                    <td>Kitsi</td>
+                    <td>2106948023</td>
+                    </tr>
+                    <tr>
+                    <td>2</td>
+                    <td>Papadopoulos</td>
+                    <td>Sotiris</td>
+                    <td>Koropi</td>
+                    <td>2103271896</td>
+                    </tr>
+                    
+                </tbody>
+                </table>
+            </div>
+        </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+  </script>
+
+<script src="assets/js/main.js"></script>
+
+<!-- ====== ionicons ======= -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
