1287506f34dc0296ea7c599993c2d07dc24bb0e6
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-17 04:54:04 +0300

    Update Flight_Search_Results.php

--- Flight_Search_Results.php
+++ Flight_Search_Results.php
@@ -122,20 +122,21 @@
     <section class="container mt-5">
         <div class="row">
             <?php
+ 
             $servername = "localhost";
             $username = "root"; 
-            $password = "";
+            $password = ""; 
             $dbname = "swiftskies"; 
-
+ 
             $conn = new mysqli($servername, $username, $password, $dbname);
-    
+
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-   
+
             $sql = "SELECT * FROM flights";
             $result = $conn->query($sql);
-    
+
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
                     ?>
@@ -143,36 +144,24 @@
                         <div class="card result-card">
                             <div class="card-body">
                                 <div class="flight-details">
-                                    <?php if(isset($row['logo'])): ?>
                                     <img src="<?php echo $row['logo']; ?>" alt="Airline Logo" class="flight-logo">
-                                    <?php endif; ?>
                                     <div>
-                                        <?php if(isset($row['flight_number'])): ?>
                                         <h5 class="card-title"><?php echo $row['flight_number']; ?></h5>
-                                        <?php endif; ?>
-                                        <?php if(isset($row['price'])): ?>
                                         <span class="flight-price">$<?php echo $row['price']; ?></span>
-                                        <?php endif; ?>
                                     </div>
                                 </div>
                                 <hr>
                                 <div class="flight-info">
-                                    <?php if(isset($row['departure']) && isset($row['destination'])): ?>
                                     <p><?php echo $row['departure']; ?></p>
                                     <i class="fa-solid fa-plane"></i>
                                     <p><?php echo $row['destination']; ?></p>
-                                    <?php endif; ?>
                                 </div>
                                 <div class="flight-info">
-                                    <?php if(isset($row['departure_time']) && isset($row['arrival_time'])): ?>
                                     <p><?php echo $row['departure_time']; ?></p>
                                     <p><?php echo $row['arrival_time']; ?></p>
-                                    <?php endif; ?>
                                 </div>
                                 <div class="flight-time">
-                                    <?php if(isset($row['flight_duration'])): ?>
                                     <p>Total Flight Time: <?php echo $row['flight_duration']; ?> hours</p>
-                                    <?php endif; ?>
                                 </div>
                                 <button class="book-button">Book Now</button>
                             </div>
